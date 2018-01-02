72693cbbb0c70bfe0e6876d972c2ddadda0d95a0
liyangKosieniMac <liyang2696@163.com>
2018-01-02 17:46:43 +0800

    Initial LYDemo

---
+++ LYDemo/LYDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYDemo
+//
+//  Created by liyang on 2018/1/2.
+//  Copyright © 2018年 kosien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYDemo/LYDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYDemo
+//
+//  Created by liyang on 2018/1/2.
+//  Copyright © 2018年 kosien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
