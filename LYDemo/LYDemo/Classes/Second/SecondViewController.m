a969b7794b23c96fbac12d65a0e2a62e5ecd8fa5
liyangKosieniMac <liyang2696@163.com>
2018-11-22 10:32:04 +0800

    commit 2018.11.22

--- LYDemo/LYDemo/Classes/Second/SecondViewController.m
+++ LYDemo/LYDemo/Classes/Second/SecondViewController.m
@@ -8,22 +8,102 @@
 
 #import "SecondViewController.h"
 
-@interface SecondViewController ()
+@interface SecondViewController ()<CLLocationManagerDelegate>
+{
+    NSMutableArray *_angleArray;
+}
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@property (nonatomic, strong) UIButton *button;
+@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UIProgressView *progress;
 
 @end
 
 @implementation SecondViewController
 
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.locationManager stopUpdatingHeading];
+    [super viewWillDisappear:animated];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"Iron Man";
+    _angleArray = [[NSMutableArray alloc] init];
     
+    _button = [Tools createButtonWithFrame:CGRectMake(100, 100, 100, 50) backgroundColor:[UIColor redColor] title:@"开始" addTarget:self action:@selector(begin)];
+    [self.view addSubview:_button];
     
+    _label = [Tools createLabelWithFrame:CGRectMake(0, 300, SCREEN_WIDTH, 50) text:nil textColor:[UIColor magentaColor] textAlignment:1];
+    _label.font = [UIFont systemFontOfSize:40];
+    [self.view addSubview:_label];
     
+    _progress = [[UIProgressView alloc] initWithFrame:CGRectMake(20, NAVBAR_HEIGHT+10, SCREEN_WIDTH-40, 10)];
+    [self.view addSubview:_progress];
+}
+
+- (CLLocationManager *)locationManager
+{
+    if (_locationManager == nil) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
+    }
+    return _locationManager;
+}
+
+- (void)begin
+{
+    if (![CLLocationManager headingAvailable]) {
+        [Tools showAlertViewOfSystemWithTitle:@"提示" andMessage:@"您的设备不支持磁力计"];
+        return;
+    }
+    
+    [_angleArray addObjectsFromArray:@[@210, @180, @220, @200, @230]];
+    _progress.progress = 0.0;
+    _button.enabled = NO;
+    [self.locationManager startUpdatingHeading];
 }
 
 
 
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    //角度
+    CGFloat angle = newHeading.magneticHeading;
+    [_button setTitle:[NSString stringWithFormat:@"%.0f", angle] forState:UIControlStateNormal];
+    
+    if (_angleArray.count == 0) {
+        _button.enabled = YES;
+        [_button setTitle:@"开始" forState:UIControlStateNormal];
+        _label.text = @"恭喜你！找到啦";
+        [self.locationManager stopUpdatingHeading];
+        return;
+    }
+    float targetAngle = [[_angleArray firstObject] floatValue];
+    
+    if (targetAngle < 180) {
+        if (angle > targetAngle && angle < targetAngle + 180) {
+            _label.text = @"<---";
+        } else {
+            _label.text = @"--->";
+        }
+        
+    } else {
+        if (angle < targetAngle && angle > targetAngle - 180) {
+            _label.text = @"--->";
+        } else {
+            _label.text = @"<---";
+        }
+    }
+    if (fabs(targetAngle - angle) < 1.0) {
+        [_angleArray removeObjectAtIndex:0];
+        [_progress setProgress:(5-_angleArray.count)/5.0 animated:YES];
+    }
+}
 
 
 @end
---
+++ LYDemo/LYDemo/Libs/ThirdPart/LYMotionManager/LYMotionManager.h
@@ -0,0 +1,28 @@
+//
+//  LYMotionManager.h
+//  LYShake
+//
+//  Created by liyang on 17/7/14.
+//  Copyright © 2017年 kosienDGL. All rights reserved.
+//  代码地址：https://github.com/YoungerLi/LYShake
+
+#import <Foundation/Foundation.h>
+#import <CoreMotion/CoreMotion.h>
+
+typedef void (^LYAccelerometerHandler)(CMAcceleration acceleration, NSError *error);
+
+/**
+ 这是一个对`CMMotionManager`进行封装的类，主要实现加速计(摇一摇)
+ */
+@interface LYMotionManager : NSObject
+
+/** 单例 */
++ (instancetype)defaultManager;
+
+/** 开始更新频率 */
+- (void)startAccelerometerUpdatesWithHandler:(LYAccelerometerHandler)handler;
+
+/** 停止更新频率 */
+- (void)stopAccelerometerUpdates;
+
+@end
